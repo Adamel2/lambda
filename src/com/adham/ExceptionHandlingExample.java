047767a892e8441951c1ff4c293d7edd22ad705d
Adham Eldda <adamel2@Adhams-MacBook-Pro.local>
2020-12-23 19:10:13 +0200

    new example

---
+++ src/com/adham/ExceptionHandlingExample.java
@@ -0,0 +1,18 @@
+package com.adham;
+
+/*
+ * @created 23/12/2020 - 19:06
+ * @author  Adham eldda
+ * @package com.adham
+ * @project lambda
+ */
+public class ExceptionHandlingExample {
+
+  public static void main(String[] args) {
+
+    int [] someNumbers = {1,2,3,4};
+
+
+
+  }
+}
